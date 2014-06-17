99740f088f7da1b7f5bce095d5a908a5d89df304
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-06-17 11:46:46 -0400

    Add test stub for Compartmental neuron class.

--- nrnengo/tests/test_neurons.py
+++ nrnengo/tests/test_neurons.py
@@ -1,9 +1,10 @@
 import nengo
+from nengo.decoders import LstsqL2
 import numpy as np
 from numpy.testing import assert_allclose
 import pytest
 
-from nrnengo.neurons import IntFire1
+from nrnengo.neurons import Compartmental, IntFire1
 
 
 def test_int_fire1_behaves_like_nengo_lif():
@@ -28,6 +29,31 @@ def test_int_fire1_behaves_like_nengo_lif():
     sim.run(2 * np.pi)
 
     assert_allclose(sim.data[B_probe], sim.data[C_probe], 0., 0.1)
+
+
+def test_compartmental():
+    model = nengo.Network(seed=1337, label="Communications Channel")
+    with model:
+        sin = nengo.Node(np.sin)
+
+        # First ensemble is needed to convert input to spikes. Otherwise the
+        # Neuron neurons cannot be connected currently.
+        A = nengo.Ensemble(50, dimensions=1)
+        B = nengo.Ensemble(50, dimensions=1, neuron_type=Compartmental())
+
+        nengo.Connection(sin, A)
+        nengo.Connection(A, B, solver=LstsqL2(weights=True))
+
+        B_probe = nengo.Probe(B, synapse=.1)
+        voltage_probe = nengo.Probe(B, 'voltage')
+
+    sim = nengo.Simulator(model)
+    sim.run(1.0)
+
+    # TODO So far this test only checks that a model with compartmental neurons
+    # can be build and simulated. It should also check that it produces sane
+    # results. However, the current implementation is not capable of doing so,
+    # yet.
 
 
 if __name__ == "__main__":
