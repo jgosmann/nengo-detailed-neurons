34a7693f6e6e95c099b7e2048fd00c5e3d06703b
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-06-16 20:52:38 -0400

    Add comments about simulator units.

--- nrnengo/neurons.py
+++ nrnengo/neurons.py
@@ -8,6 +8,8 @@ import nrn
 
 def _nrn_duration(t):
     """Converts a duration in native Nengo units to native Neuron units."""
+    # Nengo uses seconds
+    # Neuron uses milliseconds
     return 1000 * t
 
 
