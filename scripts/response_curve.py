b2281472d97d967936c3e354ff289e9a64c8aae2
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-06-20 10:21:45 -0400

    Script to generate response_curve.

---
+++ scripts/response_curve.py
@@ -0,0 +1,39 @@
+"""Script for generating the F-I-curve for stimulation in the soma and store
+the data as plot and npz file for use in the nrnengo module."""
+
+import neuron
+import numpy as np
+
+neuron.h.load_file('models/bahr2.hoc')
+
+neuron.h.dt = 0.005
+
+cell = neuron.h.Bahr2()
+
+iclamp = neuron.h.IClamp(cell.soma(0.5))
+iclamp.delay = 200
+iclamp.dur = 1000
+
+v = neuron.h.Vector()
+v.record(cell.soma(0.5)._ref_v)
+
+apcount = neuron.h.APCount(cell.soma(0.5))
+spikes = neuron.h.Vector()
+apcount.record(neuron.h.ref(spikes))
+
+current = np.linspace(0.0, 3.0, 100)
+freq = []
+
+for i in current:
+    iclamp.amp = i
+    neuron.init()
+    neuron.run(1500)
+    freq.append(len(spikes))
+
+np.savez('data/bahl2_response_curve.npz', current=current, rate=freq)
+
+import matplotlib.pyplot as plt
+plt.plot(current, freq)
+plt.xlabel("Injected current (nA, at soma)")
+plt.ylabel("Firing rate (1/s)")
+plt.savefig('plots/response_curve.pdf')
